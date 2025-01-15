1848d820459cfe046e7c7fc5491e77fffa3767c9
ChrisTho23 <christophe.thomassin23@gmail.com>
2025-01-15 17:08:06 +0100

    Initial Commit

---
+++ Code History/Code_HistoryApp.swift
@@ -0,0 +1,17 @@
+//
+//  Code_HistoryApp.swift
+//  Code History
+//
+//  Created by Christophe Thomassin on 1/15/25.
+//
+
+import SwiftUI
+
+@main
+struct Code_HistoryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
